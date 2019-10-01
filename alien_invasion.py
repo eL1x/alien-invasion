b1e8dc3bb35ac7b27e102f89e40da7fa81f70e90
eL1x <ragerrozz@gmail.com>
2019-10-01 17:40:36 +0200

    Added aliens, shooting bullets and collisons between aliens and bullets.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -9,20 +10,27 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_heigh))
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Set the background color
     bg_color = (230, 230, 230)
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)    
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
        
 
 run_game()
